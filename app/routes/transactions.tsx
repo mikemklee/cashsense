af569268c696371cca19ba34015b6afa30cfeaa7
Mike Lee <mikemunkyu.lee@gmail.com>
2025-02-23 16:31:09 -0500

    Trigger

--- app/routes/transactions.tsx
+++ app/routes/transactions.tsx
@@ -2,8 +2,6 @@ import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 
 export const action = async ({ request }: LoaderFunctionArgs) => {
   if (request.method === "POST") {
-    console.log("transactions actions - request");
-
     const payload = await request.json();
 
     // TODO: implement
